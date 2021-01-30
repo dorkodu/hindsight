5f2425d36209fce0ef6921ae25b975d34b3eaade
Doruk Dorkodu <dorkodu@gmail.com>
2021-01-30 21:53:12 +0300

    Added generateHTMLTemplate with a single role

--- source/Hindsight/Settler/SampleProject.php
+++ source/Hindsight/Settler/SampleProject.php
@@ -137,6 +137,29 @@
         )
       );
     }
+ 
 
+    /**
+     * Generates a sample HTML template file content
+     *
+     * @return void
+     */
+    private static function generateHTMLTemplate()
+    {
+      return '
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>{{ title }}</title>
+        </head>
+        <body>
+          <h1>{{ header }}</h1>
+          {{ $contents }}
+          {{ footer }}
+        </body>
+      </html>';
+    }
   }
   
