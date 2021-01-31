070bf450083b3ab5cd62bdfbbe4d4bd3838b8094
Doruk Dorkodu <dorkodu@gmail.com>
2021-01-31 10:14:40 +0300

    Will add single role methods, now getHindsightJson

--- source/Hindsight/WebsiteProject.php
+++ source/Hindsight/WebsiteProject.php
@@ -17,6 +17,24 @@
       $this->directory = realpath($directory);
     }
 
-   
+   /**
+     * Returns the 'hindsight.json' JsonFile for the project directory
+     *
+     * @return JsonFile $hindsightJson object
+     * @return false on failure
+     */
+    private function getHindsightJson()
+    {
+      if (FileStorage::isUsefulDirectory($this->projectDirectory)) {
+        # Folder is useful. Hindsight is running.
+        if ($this->isInitted()) {
+          $HindsightJson = new JsonFile($this->projectDirectory."/hindsight.json");
+          # return HindsightJson if useful
+          if ($HindsightJson->isUseful()) {
+            return $HindsightJson;
+          } else return false;
+        } else return false;
+      } else return false;
+    }
   }
   
