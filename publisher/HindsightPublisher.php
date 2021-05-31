d548f0a10246005e82ced75bf659d9ea7d84400e
Doruk Dorkodu <doruk@dorkodu.com>
2021-05-31 13:23:39 +0300

    Update HindsightPublisher.php

--- publisher/HindsightPublisher.php
+++ publisher/HindsightPublisher.php
@@ -1,78 +1,78 @@
 <?php
-  require "TerminalUI.php";
-  require "CLITinkerer.php";
-  require "PharPackager.php";
-  require "Timer.php";
+require "TerminalUI.php";
+require "CLITinkerer.php";
+require "PharPackager.php";
+require "Timer.php";
 
-  class HindsightPublisher
+class HindsightPublisher
+{
+  public const PKG_NAME = "hindsight";
+
+  public static function publish(string $sourceFolder, string $publishFolder)
   {
-    public const PKG_NAME = "hindsight";
-    
-    public static function publish(string $sourceFolder, string $publishFolder)
-    {
-      self::consoleLog("This will build, run tests and publish Hindsight project.");
-      self::removePreviousPackageIfExists($publishFolder);
+    self::consoleLog("This will build, run tests and publish Hindsight project.");
+    self::removePreviousPackageIfExists($publishFolder);
 
-      $hindsightPhar = new PharPackager('hindsight.phar', $sourceFolder, $publishFolder);
-      $hindsightPhar->setDefaultStub("bootstrap.php");
-      $hindsightPhar->publish();
+    $hindsightPhar = new PharPackager('hindsight.phar', $sourceFolder, $publishFolder);
+    $hindsightPhar->setDefaultStub("bootstrap.php");
+    $hindsightPhar->publish();
 
-      $successPublished = is_file("hindsight.phar");
-      $successRename = rename("hindsight.phar", self::PKG_NAME);
-      $successCopy = copy(self::PKG_NAME, "sample/" . self::PKG_NAME);
-      
-      if ( $successPublished && $successRename && $successCopy) {
-        self::consoleLog("Successfully published the Hindsight.");
+    $successPublished = is_file("hindsight.phar");
+    $successRename = rename("hindsight.phar", self::PKG_NAME);
+    $successCopy = copy(self::PKG_NAME, "sample/" . self::PKG_NAME);
 
-        $resultOutput = self::runTests();
+    if ($successPublished && $successRename && $successCopy) {
+      self::consoleLog("Successfully published the Hindsight.");
 
-        self::consoleLog("Test Results :");
-        printf("%s\n\n", $resultOutput);
-    
-        self::consoleLog("All tests have been run.");
-        self::consoleLog("DONE.");
-      } else throw new Exception("An unknown problem occured on publishing Hindsight.");
-    }
+      $resultOutput = self::runTests();
 
-    public static function packageAlreadyExists($directory)
-    {
-      return is_file(realpath($directory)."hindsight");
-    }
+      self::consoleLog("Test Results :");
+      printf("%s\n\n", $resultOutput);
 
-    public static function removePreviousPackageIfExists($directory)
-    {
-      if(self::packageAlreadyExists($directory)) {
-        return unlink(realpath($directory)."/hindsight");
-      }
-    }
+      self::consoleLog("All tests have been run.");
+      self::consoleLog("DONE.");
+    } else throw new Exception("An unknown problem occured on publishing Hindsight.");
+  }
 
-    /**
-     * Runs all Hindsight tests and returns the output
-     *
-     * @return string
-     */
-    public static function runTests() 
-    {
-      $output = array();
-      $result = null;
-      exec("php test/test.php", $output, $result);
+  public static function packageAlreadyExists($directory)
+  {
+    return is_file(realpath($directory) . "hindsight");
+  }
 
-      return implode(PHP_EOL, $output);
-    }
-
-    public static function consoleLog($text)
-    {
-      TerminalUI::bold("Hindsight Publisher");
-      CLITinkerer::write(" > ". $text);
-      CLITinkerer::breakLine();
-    }
-
-    public static function breakRunning($topic, $content)
-    {
-      TerminalUI::bold("Hindsight Publisher > ");
-      TerminalUI::bold($topic);
-      CLITinkerer::write(": ".$content);
-      CLITinkerer::breakLine();
-      exit;
+  public static function removePreviousPackageIfExists($directory)
+  {
+    if (self::packageAlreadyExists($directory)) {
+      return unlink(realpath($directory) . "/hindsight");
     }
   }
+
+  /**
+   * Runs all Hindsight tests and returns the output
+   *
+   * @return string
+   */
+  public static function runTests()
+  {
+    $output = array();
+    $result = null;
+    exec("php test/test.php", $output, $result);
+
+    return implode(PHP_EOL, $output);
+  }
+
+  public static function consoleLog($text)
+  {
+    TerminalUI::bold("Hindsight Publisher");
+    CLITinkerer::write(" > " . $text);
+    CLITinkerer::breakLine();
+  }
+
+  public static function breakRunning($topic, $content)
+  {
+    TerminalUI::bold("Hindsight Publisher > ");
+    TerminalUI::bold($topic);
+    CLITinkerer::write(": " . $content);
+    CLITinkerer::breakLine();
+    exit;
+  }
+}
