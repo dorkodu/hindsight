21708a9f30bd98c3d00b1e2aeedff5bb60c52d00
Doruk Dorkodu <doruk@dorkodu.com>
2021-05-31 17:21:10 +0300

    Update test.php

--- test/test.php
+++ test/test.php
@@ -1,9 +1,10 @@
 <?php
-  require __DIR__ . '/HindsightTest.php';
-  
-  error_reporting(E_ERROR);
 
-  // this is how to use it.
-  $test = new HindsightTest();
-  $test->runTests();
-  $test->seeTestResults();
+require __DIR__ . '/HindsightTest.php';
+
+error_reporting(E_ERROR);
+
+// this is how to use it.
+$test = new HindsightTest();
+$test->runTests();
+$test->seeTestResults();
