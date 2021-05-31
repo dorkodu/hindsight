12f8d3c6c8cfae5a3b9b1b75014f1677803826e9
Doruk Dorkodu <doruk@dorkodu.com>
2021-05-31 13:14:39 +0300

    Update CLITinkerer.php

--- publisher/CLITinkerer.php
+++ publisher/CLITinkerer.php
@@ -1,51 +1,50 @@
 <?php
-  class CLITinkerer
+class CLITinkerer
+{
+  public static function readLine($optionalMessage = null)
   {
-    public static function readLine($optionalMessage = null)
-    {
-      $fh = fopen('php://stdin', 'r');
-      if (!empty($optionalMessage))  {
-        echo $optionalMessage." ";        
-      }
-      $userInput = trim(fgets($fh));
-      fclose($fh);
-      unset($fh);
+    $fh = fopen('php://stdin', 'r');
+    if (!empty($optionalMessage)) {
+      echo $optionalMessage . " ";
+    }
+    $userInput = trim(fgets($fh));
+    fclose($fh);
+    unset($fh);
 
-      return $userInput;
-    }
-    
-    public static function writeLine($message)
-    {
-      echo $message.PHP_EOL;
-    }
-
-    public static function breakLine()
-    {
-      echo PHP_EOL;
-    }
-
-    public static function write($message)
-    {
-      echo $message;
-    }
-
-    public static function getScriptName()
-    {
-      return $_SERVER['argv'][0];
-    }
-
-    public static function argumentCount()
-    {
-      return $_SERVER['argc'];
-    }
-
-    public static function getArgument($index)
-    {
-      if (is_int($index)) {
-        if(isset($_SERVER['argv'][$index]) && !empty($_SERVER['argv'][$index])) {
-          return $_SERVER['argv'][$index];
-        } else return false;
-      } else return false;
-    }
+    return $userInput;
   }
-  
+
+  public static function writeLine($message)
+  {
+    echo $message . PHP_EOL;
+  }
+
+  public static function breakLine()
+  {
+    echo PHP_EOL;
+  }
+
+  public static function write($message)
+  {
+    echo $message;
+  }
+
+  public static function getScriptName()
+  {
+    return $_SERVER['argv'][0];
+  }
+
+  public static function argumentCount()
+  {
+    return $_SERVER['argc'];
+  }
+
+  public static function getArgument($index)
+  {
+    if (is_int($index)) {
+      if (isset($_SERVER['argv'][$index]) && !empty($_SERVER['argv'][$index])) {
+        return $_SERVER['argv'][$index];
+      } else return false;
+    } else return false;
+  }
+}
